c0f902e9a5e202c255a4715176680b07d43ebb1c
Adam Smith <asmith13194@gmail.com>
2017-08-23 10:24:01 -0600

    add animations

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+// const ads = require('./routes/classifieds');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+// const morgan = require('morgan');
+// const cors = require('cors');
+const port = process.env.PORT || 8000;
+
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config();
+// }
+
+// app.use(morgan());
+// app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.use(cors())
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.use(express.static('public'));
+
+app.use('/', (req,res,next)=>{
+  res.sendStatus(404);
+});
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+
+// module.exports = app;
