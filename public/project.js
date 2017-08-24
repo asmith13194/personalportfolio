5ff9e10c174874362b315438e3543d5f9388bced
Adam Smith <asmith13194@gmail.com>
2017-08-24 15:28:40 -0600

    good stuff

--- public/project.js
+++ public/project.js
@@ -1,141 +1,139 @@
-$(document).ready(function(){
-  $("#tanks-container").hover(function(){
-    $("#image-tanks").css("display","none")
-    $("#tanks-project-detail-container").css('visibility','visible')
-    $("#tanks-close-button").css('visibility','visible')
+$(document).ready(function() {
+  $("#tanks-container").hover(function() {
+    $("#image-tanks").css("display", "none")
+    $("#tanks-project-detail-container").css('visibility', 'visible')
+    $("#tanks-close-button").css('visibility', 'visible')
     $('#tanks-content').addClass('slideExpandUp')
-  },function(){
-    $('#tanks-content').css('visibility','hidden');
-    $(".project-detail-container").css('visibility','hidden')
+  }, function() {
+    $('#tanks-content').css('visibility', 'hidden');
+    $(".project-detail-container").css('visibility', 'hidden')
     $('#tanks-content').removeClass('slideExpandUp');
-    $("#image-tanks").css("display","inline")
+    $("#image-tanks").css("display", "inline")
   })
-  $("#muzica-container").hover(function(){
-    $("#image-muzica").css("display","none")
-    $("#muzica-project-detail-container").css('visibility','visible')
-    $("#muzica-close-button").css('visibility','visible')
+  $("#muzica-container").hover(function() {
+    $("#image-muzica").css("display", "none")
+    $("#muzica-project-detail-container").css('visibility', 'visible')
+    $("#muzica-close-button").css('visibility', 'visible')
     $('#muzica-content').addClass('slideExpandUp');
-  },function(){
-    $('#muzica-content').css('visibility','hidden');
-    $(".project-detail-container").css('visibility','hidden')
+  }, function() {
+    $('#muzica-content').css('visibility', 'hidden');
+    $(".project-detail-container").css('visibility', 'hidden')
     $('#muzica-content').removeClass('slideExpandUp');
-    $("#image-muzica").css("display","inline")
+    $("#image-muzica").css("display", "inline")
   })
-  $("#whatsup-container").hover(function(){
-    $("#image-whatsup").css("display","none")
-    $("#whatsup-project-detail-container").css('visibility','visible')
-    $("#whatsup-close-button").css('visibility','visible')
+  $("#whatsup-container").hover(function() {
+    $("#image-whatsup").css("display", "none")
+    $("#whatsup-project-detail-container").css('visibility', 'visible')
+    $("#whatsup-close-button").css('visibility', 'visible')
     $('#whatsup-content').addClass('slideExpandUp');
-  },function(){
-    $('#whatsup-content').css('visibility','hidden');
-    $(".project-detail-container").css('visibility','hidden')
+  }, function() {
+    $('#whatsup-content').css('visibility', 'hidden');
+    $(".project-detail-container").css('visibility', 'hidden')
     $('#whatsup-content').removeClass('slideExpandUp');
-    $("#image-whatsup").css("display","inline")
+    $("#image-whatsup").css("display", "inline")
   })
-  $("#pixelart-container").hover(function(){
-    $("#image-pixelart").css("display","none")
-    $("#pixelart-project-detail-container").css('visibility','visible')
-    $("#pixelart-close-button").css('visibility','visible')
+  $("#pixelart-container").hover(function() {
+    $("#image-pixelart").css("display", "none")
+    $("#pixelart-project-detail-container").css('visibility', 'visible')
+    $("#pixelart-close-button").css('visibility', 'visible')
     $('#pixelart-content').addClass('slideExpandUp');
-  },function(){
-    $('#pixelart-content').css('visibility','hidden');
-    $(".project-detail-container").css('visibility','hidden')
+  }, function() {
+    $('#pixelart-content').css('visibility', 'hidden');
+    $(".project-detail-container").css('visibility', 'hidden')
     $('#pixelart-content').removeClass('slideExpandUp');
-    $("#image-pixelart").css("display","inline")
+    $("#image-pixelart").css("display", "inline")
   })
-  $("#jokebook-container").hover(function(){
-    $("#image-jokebook").css("display","none")
-    $("#jokebook-project-detail-container").css('visibility','visible')
-    $("#jokebook-close-button").css('visibility','visible')
+  $("#jokebook-container").hover(function() {
+    $("#image-jokebook").css("display", "none")
+    $("#jokebook-project-detail-container").css('visibility', 'visible')
+    $("#jokebook-close-button").css('visibility', 'visible')
     $('#jokebook-content').addClass('slideExpandUp');
-  },function(){
-    $('#jokebook-content').css('visibility','none');
-    $(".project-detail-container").css('visibility','hidden')
+  }, function() {
+    $('#jokebook-content').css('visibility', 'none');
+    $(".project-detail-container").css('visibility', 'hidden')
     $('#jokebook-content').removeClass('slideExpandUp');
-    $("#image-jokebook").css("display","inline")
+    $("#image-jokebook").css("display", "inline")
   })
-  function getRandom(arr){
-    return arr[Math.floor(Math.random()*arr.length)];
+  function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
   }
-  let quotesArr = ['"Science and technology revolutionize our lives, but memory, tradition and myth frame our response." - Arthur Schlesinger','"The art challenges the technology, and the technology inspires the art." - John Lasseter','"If we continue to develop our technology without wisdom or prudence, our servant may prove to be our executioner." - Omar Bradley','"The real danger is not that computers will begin to think like men, but that men will begin to think like computers." - Sydney Harris','"This is the whole point of technology.  It creates an appetite for immortality on the one hand.  It threatens universal extinction on the other. Technology is lust removed from nature." - Don DeLillo','"Technology made large populations possible; large populations now make technology indispensable." - Joseph Krutch','"We are stuck with technology when what we really want is just stuff that works." - Douglas Adams','"The great myth of our times is that technology is communication." - Libby Larsen','"I think that novels that leave out technology misrepresent life as badly as Victorians misrepresented life by leaving out sex." - Kurt Vonnegut','"The human spirit must prevail over technology." - Albert Einstein','"You cannot endow even the best machine with initiative; the jolliest steamroller will not plant flowers." - Walter Lippmann','"Humanity is acquiring all the right technology for all the wrong reasons." - R. Buckminster Fuller','"Technology is a word that describes something that doesn’t work yet." - Douglas Adams','"One machine can do the work of fifty ordinary men.  No machine can do the work of one extraordinary man." - Elbert Hubbard','"It has become appallingly obvious that our technology has exceeded our humanity." - Albert Einstein','"Just because something doesn’t do what you planned it to do doesn’t mean it’s useless." - Thomas Edison','"All of the biggest technological inventions created by man - the airplane, the automobile, the computer - says little about his intelligence, but speaks volumes about his laziness." - Mark Kennedy','"Any sufficiently advanced technology is equivalent to magic." - Arthur C. Clarke','"Technology is anything that wasn’t around when you were born." - Alan Kay']
-  setTimeout(function(){
+  let quotesArr = [
+    '"Science and technology revolutionize our lives, but memory, tradition and myth frame our response." - Arthur Schlesinger',
+    '"The art challenges the technology, and the technology inspires the art." - John Lasseter',
+    '"If we continue to develop our technology without wisdom or prudence, our servant may prove to be our executioner." - Omar Bradley',
+    '"The real danger is not that computers will begin to think like men, but that men will begin to think like computers." - Sydney Harris',
+    '"This is the whole point of technology.  It creates an appetite for immortality on the one hand.  It threatens universal extinction on the other. Technology is lust removed from nature." - Don DeLillo',
+    '"Technology made large populations possible; large populations now make technology indispensable." - Joseph Krutch',
+    '"We are stuck with technology when what we really want is just stuff that works." - Douglas Adams',
+    '"The great myth of our times is that technology is communication." - Libby Larsen',
+    '"I think that novels that leave out technology misrepresent life as badly as Victorians misrepresented life by leaving out sex." - Kurt Vonnegut',
+    '"The human spirit must prevail over technology." - Albert Einstein',
+    '"You cannot endow even the best machine with initiative; the jolliest steamroller will not plant flowers." - Walter Lippmann',
+    '"Humanity is acquiring all the right technology for all the wrong reasons." - R. Buckminster Fuller',
+    '"Technology is a word that describes something that doesn’t work yet." - Douglas Adams',
+    '"One machine can do the work of fifty ordinary men.  No machine can do the work of one extraordinary man." - Elbert Hubbard',
+    '"It has become appallingly obvious that our technology has exceeded our humanity." - Albert Einstein',
+    '"Just because something doesn’t do what you planned it to do doesn’t mean it’s useless." - Thomas Edison',
+    '"All of the biggest technological inventions created by man - the airplane, the automobile, the computer - says little about his intelligence, but speaks volumes about his laziness." - Mark Kennedy',
+    '"Any sufficiently advanced technology is equivalent to magic." - Arthur C. Clarke',
+    '"Technology is anything that wasn’t around when you were born." - Alan Kay'
+  ]
+  setTimeout(function() {
     changeQuote()
   }, 7000);
-   function changeQuote(){
+  function changeQuote() {
     let randomQuote = getRandom(quotesArr);
     $('#quote-text').html(randomQuote);
-    $('#quote-text').css('visibility','hidden');
+    $('#quote-text').css('visibility', 'hidden');
     $('#quote-text').addClass('slideLeft');
-    setTimeout(function(){
+    setTimeout(function() {
       $('#quote-text').removeClass('slideLeft');
-      setTimeout(function(){
+      setTimeout(function() {
         changeQuote();
-      },200)
-    },7000)
-   }
-  $('#skills-container').hover(function(){
-    $('#skills-title').css('display','none')
+      }, 200)
+    }, 7000)
+  }
+  $('#skills-container').mouseover(function() {
+    $('#skills-container').removeClass('slideRight')
+    $('#skills-title').css('visibility', 'hidden')
     $('#skills-title').removeClass('fadeIn')
-    $('#skills-content').css('display','inline')
-    $('#skills-content').addClass('slideRight');
-  },function(){
-    $('#skills-content').removeClass('slideExpandUp')
-    $('#skills-content').css('visibility','hidden')
-    $('#skills-content').css('display','none')
-    $('#skills-title').css('display','inline')
-    $('#skills-title').css('visibility','hidden')
-    $('#skills-title').addClass('fadeIn')
+    $('#skills-content').css('display', 'block')
+    setTimeout(function() {
+      $('.skill-item').css('display', 'block')
+      $('#skills-title').addClass('slideRight');
+      $('.skill-item').addClass('slideRight');
+    }, 200)
   })
-  function expandUp(){
+  function expandUp() {
     $('.skills1').addClass('expandUp')
   }
-  $('#about-container').hover(function(){
-    $('#about-title').css('display','none')
+  $('#about-container').mouseover(function() {
+    $('#about-container').removeClass('slideExpandUp')
+    $('#about-title').css('visibility', 'hidden')
     $('#about-title').removeClass('fadeIn')
-    $('#about-content').css('display','inline')
-    $('#about-content').addClass('slideExpandUp');
-  },function(){
-    $('#about-content').removeClass('slideExpandUp')
-    $('#about-content').css('visibility','hidden')
-    $('#about-content').css('display','none')
-    $('#about-title').css('display','inline')
-    $('#about-title').css('visibility','hidden')
-    $('#about-title').addClass('fadeIn')
+    setTimeout(function() {
+      $('#about-content').css('display', 'inline')
+      $('#about-title').addClass('slideExpandUp');
+      $('.aboutme-container').addClass('slideExpandUp');
+    }, 300)
   })
-  function slideExpandUp(){
-    $('#about-content').css('visibility','hidden')
-    $('#about-content').removeclass('slideExpandUp')
-  }
-  $('#contact-container').hover(function(){
-    $('#contact-title').css('display','none')
+  $('#contact-container').mouseover(function() {
+    $('#contact-article').removeClass('slideDown')
+    $('#contact-title').css('visibility', 'hidden')
     $('#contact-title').removeClass('fadeIn')
-    $('#contact-content').css('display','inline')
-    $('#contact-content').addClass('slideDown');
-  },function(){
-    $('#contact-content').removeClass('slideDown')
-    $('#contact-content').css('visibility','hidden')
-    $('#contact-content').css('display','none')
-    $('#contact-title').css('display','inline')
-    $('#contact-title').css('visibility','hidden')
-    $('#contact-title').addClass('fadeIn')
+    setTimeout(function() {
+      $('#contact-content').css('display', 'inline')
+      $('#contact-title').addClass('slideDown');
+      $('.contact-info').addClass('slideDown');
+    }, 300)
   })
-  function slideDown(){
-    $('.contact-container').addClass('slideDown')
-  }
-  $('#picture-container').hover(function(){
-    $('#selfie').css('visibility','hidden');
-    $('#selfie').removeClass('fadeIn')
-    window.setTimeout(fadeIn,100);
-  },function(){})
-  function fadeIn(){
-    $('#selfie').addClass('fadeIn')
-  }
-  $('#project-container').hover(function(){
-    $('#project-title').css('visibility','hidden');
-    $('#project-title').removeClass('hatch')
+  $('#picture-container').mouseover(function() {
+    window.setTimeout(function() {
+      $('#selfie').addClass('fadeIn')
+    }, 100);
+  })
+  function fadeIn() {}
+  $('#project-container').mouseover(function() {
     $('.image-container').addClass('hatch');
-  },function(){
-    $('#project-title').addClass('hatch')
-    $('.image-container').removeClass('hatch');
   })
 });
--- server.js
+++ server.js
@@ -2,29 +2,7 @@
 
 const express = require('express');
 const app = express();
-// const ads = require('./routes/classifieds');
-// const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-// const morgan = require('morgan');
-// const cors = require('cors');
 const port = process.env.PORT || 8000;
-
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config();
-// }
-
-// app.use(morgan());
-// app.use(cookieParser());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
-// app.use(cors())
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 
 app.use(express.static('public'));
 
